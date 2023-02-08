324b9d7a4fb46b985e272c11793bba90a7d56a1c
QodirovDoston <qodirovdoston806@gmail.com>
2023-02-08 17:40:18 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -9,14 +9,12 @@ function App() {
   return (
 
     <>
-    <Router>
       <Navbarfunc />
       <Routes>
         <Route path="/home" element={<Header />} />
         <Route path="/result" element={<Getdata />} />
         <Route path="/result/edite:id" element={<Edit/>} />
       </Routes>
-    </Router>
     </>
   );
 }
